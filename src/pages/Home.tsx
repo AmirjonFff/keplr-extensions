44133d8c768f6561411b1bbdf59c8e498b0c0738
Aminjon Ffff <fayzmuhammadamirjoni@gmail.com>
2025-08-20 16:10:07 +0500

    home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,44 @@
-export default function Home() {
-    return <h1 className="text-2xl font-bold">Home Page</h1>;
-}
+import '../../public/assets/css/normalize.css'
+import '../../public/assets/css/style.css'
+import headerLogo from '../../public/assets/img/header-logo.svg';
+import introVideo from '../../public/assets/intro.webm';
+
+const Home = () => {
+    return (
+        <div className="home">
+            <div className="header">
+                <img src={headerLogo} alt="logo" className="header__logo" />
+                <div className="header__desc">Your Interchain Gateway</div>
+            </div>
+
+            <div className="container home-container">
+                <div className="home__logo">
+                    <video
+                        width="200"
+                        height="200"
+                        autoPlay
+                        loop
+                        muted
+                        className="home-video"
+                    >
+                        <source src={introVideo} type="video/webm" />
+                    </video>
+                </div>
+
+                <div className="home__btns">
+                    <a href="/login" className="btn btn--one">
+                        Create a new wallet
+                    </a>
+                    <a href="/login" className="btn btn--two">
+                        Import an existing wallet
+                    </a>
+                    <a href="/login" className="btn btn--three">
+                        Connect Hardware Wallet
+                    </a>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
